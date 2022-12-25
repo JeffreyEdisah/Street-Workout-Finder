93a391fdd629c753cd386010e592ded78238be9a
Thibault Adelain <94785843+ThibaultAdelain@users.noreply.github.com>
2022-12-25 19:10:42 +0100

    Delete backend directory

--- backend/middleware/authMiddleware.js
+++
@@ -1,8 +0,0 @@
-const asyncHandler = require('express-async-handler')
-const User = require('../models/userModel')
-
-const protect = asyncHandler(async (req, res, next) => {
-    next()
-})
-
-module.exports = { protect }
