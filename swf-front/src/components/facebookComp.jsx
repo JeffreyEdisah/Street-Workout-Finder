db6c05e70dbd20bd42a9ca6ba561950cd7d56e41
Thibault Adelain <thibault.adelain@gmail.com>
2023-02-01 10:47:32 +0100

    login page

---
+++ swf-front/src/components/facebookComp.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function facebookComp(props) {
+  return (
+    <div className={props.className}>
+        <svg width="38" height="42" viewBox="0 0 38 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_332_296)">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M0.684448 21.1082C0.684448 31.3436 7.25774 39.8548 15.8547 41.5808V26.7114H11.3037V20.9933H15.8547V16.4177C15.8547 11.2708 18.7872 8.41258 22.9347 8.41258C24.2485 8.41258 25.6654 8.64076 26.9791 8.86894V14.1308H24.6535C22.428 14.1308 21.9229 15.3883 21.9229 16.9907V20.9933H26.7774L25.9688 26.7114H21.9229V41.5808C30.5199 39.8548 37.0932 31.3453 37.0932 21.1082C37.0932 9.72161 28.9012 0.405762 18.8888 0.405762C8.87641 0.405762 0.684448 9.72161 0.684448 21.1082Z" fill="white"/>
+                </g>
+                <defs>
+                <clipPath id="clip0_332_296">
+                <rect width="36.4087" height="41.175" fill="white" transform="translate(0.684448 0.405762)"/>
+                </clipPath>
+                </defs>
+        </svg>
+    </div>
+  )
+}
+
+export default facebookComp
