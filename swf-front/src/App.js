aeb8511b7be0e41b59625badd188b5fda28f84ad
ThomasDGH <116448822+ThomasDGH@users.noreply.github.com>
2023-03-13 17:24:03 +0100

    ajout route menu

--- swf-front/src/App.js
+++ swf-front/src/App.js
@@ -2,22 +2,27 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom' 
 
 // Components
-import MapCompontent from './components/MapComponent';
-import MapCompontentTest from './components/MapComponentTest';
+import MapComponent from './components/MapComponent'
 import Login from './pages/Login';
 import Register from './pages/Register';
-import menuComponent from './components/menuComponent';
+import Logout from './pages/Logout';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import Menu from './components/MenuComponent';
 
 function App() {
   return (
     <>
+    <Provider store={store}>
       <Router>
         <Routes>
-          <Route path='/' element={<MapCompontentTest />}/>
+          <Route path='/' element={<MapComponent />}/>
           <Route path='/login' element={<Login />}/>
           <Route path='/register' element={<Register />}/>
+          <Route path='/menu' element={<Menu />}/>
         </Routes>
       </Router>
+    </Provider>
     </>
   );
 }
